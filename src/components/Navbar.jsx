3e4e19bcf8d23640811a1ea2a6639eac823794f0
Sebastianidm <sebastiandiazmiranda@gmail.com>
2023-01-18 15:48:00 -0300

    aplicando links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,9 @@ const Navbar = () => {
   const enviarCorreo = () => {
     location.href ='mailto:sebastiandiazmiranda@gmail.com';
   }
+  const irArepos = ( ) => {
+    window.location.replace("https://github.com/Sebastianidm");
+  }
   return (
     <div>
       <header className="bg-gray-800 md:sticky top-0 z-10">
@@ -14,9 +17,9 @@ const Navbar = () => {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#Github" className="mr-5 hover:text-white">
+          <button onClick={ irArepos} href="#Github" className="mr-5 hover:text-white">
             Mis proyectos
-          </a>
+          </button>
           <a href="#skills" className="mr-5 hover:text-white">
             Habilidades
           </a>
