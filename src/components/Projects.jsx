85bcd9955a4b41c3ecaa136aee5e42d9dcc1b7a1
Sebastianidm <sebastiandiazmiranda@gmail.com>
2023-02-14 22:19:21 -0300

    sintaxi y cambios varios

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ export default function Projects() {
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
                 
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-100 ">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 ">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
