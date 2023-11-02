3e0c07ef69b0d106f8aa00d79b8c049736a5b7d1
Sebastianidm <sebastiandiazmiranda@gmail.com>
2023-11-01 21:39:43 -0300

    fix menor en parrafo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,56 +1,47 @@
-import React from 'react'
-import {Fade} from "react-awesome-reveal"
-import imagenPerfil from '../img/imagen-perfil.jpg';
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+import imagenPerfil from "../img/imagen-perfil.jpg";
 import { Link } from "react-router-dom";
 const About = () => {
-  const irArepos = ( ) => {
+  const irArepos = () => {
     window.location.replace("https://github.com/Sebastianidm");
-  }
+  };
   return (
-    <section id='about' className=' bg-slate-800' >
-      
+    <section id="about" className=" bg-slate-800">
       <div className="container aboutStyle  pb-14 mx-auto flex px-10 flex-col items-center justify-center ">
-
-      <img src={imagenPerfil} alt='perfiljpg' className='fotoPerfil rounded-full border-transparent border-2 ' />
+        <img
+          src={imagenPerfil}
+          alt="perfiljpg"
+          className="fotoPerfil rounded-full border-transparent border-2 "
+        />
 
         <div className=" font-mono  sm:w-1/2   flex flex-col sm:items-center sm:text-center sm-16 sm:mb-0 items-center text-center">
-              
-             
-
-           <Fade cascade >   
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mx-0">
-            Hola, soy Sebastián 
-            <br className="hidden lg:inline-block" />Mi pasión es construir aplicaciones front-end.
-          </h1>
+          <Fade cascade>
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mx-0">
+              Hola, soy Sebastián mi pasión es construir aplicaciones front-end.
+            </h1>
           </Fade>
-          <p className="mb-8 leading-relaxed">
-           
-          </p>
+          <p className="mb-8 leading-relaxed"></p>
           <div className="flex justify-center">
             <Link to="contact">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none rounded text-lg">
-              Trabaja conmigo
-            </a>
+              <a
+                href="#contact"
+                className="inline-flex text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none rounded text-lg"
+              >
+                Trabaja conmigo
+              </a>
             </Link>
             <button
-              
               onClick={irArepos}
-              className="ml-4 inline-flex  bg-black text-white border-0 py-2 px-6 focus:outline-none  rounded text-lg">
+              className="ml-4 inline-flex  bg-black text-white border-0 py-2 px-6 focus:outline-none  rounded text-lg"
+            >
               Mira mi Github
             </button>
           </div>
-
         </div>
-        
-       
-        
       </div>
-
-     
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
