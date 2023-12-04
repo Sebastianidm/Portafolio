fc9fd8453dda82b2385912394d908f74940ec017
Sebastianidm <sebastiandiazmiranda@gmail.com>
2023-12-04 13:40:11 -0300

    agregar vista 404

--- src/Views/NotFoundView.jsx
+++ src/Views/NotFoundView.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 
 
 const NotFoundView = () => {
   return (
-    <section className="flex items-center h-screen p-16 bg-gray-900 text-ligth-blue">
+    <section className="flex items-center h-screen p-16 bg-slate-800 text-white">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
           <h2 className="mb-8 font-extrabold text-9xl text-ligth-blue">
@@ -13,7 +13,9 @@ const NotFoundView = () => {
           <p className="text-2xl font-semibold md:text-3xl pb-4">
             Lo sentimos, pero esta pagina no esta disponile.
           </p>
-          <button className="px-8 py-3 font-semibold rounded bg-bright-blue text-gray-900"> Volver al home </button>
+          <Link to="/">
+          <button className="inline-flex text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none rounded text-lg transform transition-transform hover:scale-105"> Volver al home </button>
+          </Link>
         </div>
       </div>
     </section>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -37,7 +37,11 @@ export const router = createBrowserRouter([
             {
                 path: "contact",
                 element: <Contact/>
-            }
+            },
+            {
+                path: "*",
+                element: <NotFoundView />,
+            },
 
         ]
     },
